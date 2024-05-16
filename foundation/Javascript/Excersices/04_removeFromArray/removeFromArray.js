eaf30ed913af9a8881af9de1f4fb7fd39351e0fa
jhansali <jhansali@asu.edu>
2024-05-16 09:04:04 -0700

    Javascript: excersices

---
+++ foundation/Javascript/Excersices/04_removeFromArray/removeFromArray.js
@@ -0,0 +1,12 @@
+const removeFromArray = function(arr, ...args) {
+    const newArray = [];
+    arr.forEach(element => {
+       if(!args.includes(element)) {
+           newArray.push(element);
+       }
+    });
+    return newArray;
+};
+
+// Do not edit below this line
+module.exports = removeFromArray;
---
+++ foundation/Javascript/Excersices/05_sumAll/sumAll.js
@@ -0,0 +1,16 @@
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    let total=0;
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        total += i;
+    }
+    return total;
+};
+
+// Do not edit below this line
+module.exports = sumAll;
